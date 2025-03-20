302ffa24edbe741267b2a359cf400c68eda15ab5
Isamu Mogi <isamu@leafytree.jp>
2025-03-20 14:33:00 +0900

    chore: prettier

--- .devcontainer/update_content_command.bash
+++ .devcontainer/update_content_command.bash
@@ -7,3 +7,9 @@ git -C ~/.rbenv/plugins/ruby-build pull
 
 sudo apt-get update -qq
 sudo apt-get dist-upgrade -y
+
+# https://github.com/Schniz/fnm/blob/v1.38.1/README.md?plain=1#L35-L39
+curl --fail --show-error --location https://fnm.vercel.app/install | bash -s -- --skip-shell
+bash -ic "fnm install"
+
+bash -ic "npm install --global prettier"
