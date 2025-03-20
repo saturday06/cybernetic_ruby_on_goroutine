cc8d729d32b9f1dc0d933b759440a396228f96cc
Isamu Mogi <isamu@leafytree.jp>
2025-03-20 15:28:41 +0900

    chore: print

--- ext/cybernetic_ruby_on_goroutine/cybernetic_ruby_on_goroutine.go
+++ ext/cybernetic_ruby_on_goroutine/cybernetic_ruby_on_goroutine.go
@@ -6,12 +6,15 @@ package main
 import "C"
 
 import (
+	"fmt"
+
 	"github.com/ruby-go-gem/go-gem-wrapper/ruby"
 )
 
 //export Init_cybernetic_ruby_on_goroutine
 func Init_cybernetic_ruby_on_goroutine() {
 	rb_mCyberneticRubyOnGoroutine := ruby.RbDefineModule("CyberneticRubyOnGoroutine")
+	fmt.Printf("rb_mCyberneticRubyOnGoroutine: %v\n", rb_mCyberneticRubyOnGoroutine)
 }
 
 func main() {
