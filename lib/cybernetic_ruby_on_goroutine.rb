71d43866db816fab80b1cfb7cd6b821127fcd209
Isamu Mogi <isamu@leafytree.jp>
2025-03-20 14:04:33 +0900

    chore: 世紀末

--- lib/cybernetic_ruby_on_goroutine.rb
+++ lib/cybernetic_ruby_on_goroutine.rb
@@ -5,6 +5,7 @@ require_relative "cybernetic_ruby_on_goroutine/version"
 module CyberneticRubyOnGoroutine
   class Error < StandardError; end
 
+  # これは世紀末
   class EndOfTheCentury
     def process
       puts "ok"
