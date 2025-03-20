8e4b1fdc8aa42fcbc3dfdfbde2370ce6e2c0ae12
Isamu Mogi <isamu@leafytree.jp>
2025-03-20 22:00:36 +0000

    chore: a

--- ext/cybernetic_ruby_on_goroutine/cybernetic_ruby_on_goroutine.go
+++ ext/cybernetic_ruby_on_goroutine/cybernetic_ruby_on_goroutine.go
@@ -7,7 +7,15 @@ VALUE rb_cybernetic_ruby_on_goroutine(VALUE self, VALUE a, VALUE b);
 */
 import "C"
 
-import "github.com/ruby-go-gem/go-gem-wrapper/ruby"
+import (
+	"fmt"
+	"runtime"
+	"syscall"
+	"time"
+	"unsafe"
+
+	"github.com/ruby-go-gem/go-gem-wrapper/ruby"
+)
 
 //export Init_cybernetic_ruby_on_goroutine
 func Init_cybernetic_ruby_on_goroutine() {
@@ -17,12 +25,38 @@ func Init_cybernetic_ruby_on_goroutine() {
 
 //export rb_cybernetic_ruby_on_goroutine
 func rb_cybernetic_ruby_on_goroutine(_ C.VALUE, a C.VALUE, b C.VALUE) C.VALUE {
-	aLong := ruby.NUM2LONG(ruby.VALUE(a))
-	bLong := ruby.NUM2LONG(ruby.VALUE(b))
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+	fmt.Println("start", syscall.Gettid())
 
-	sum := aLong + bLong
+	done := func() <-chan struct{} {
+		done := make(chan struct{})
 
-	return C.VALUE(ruby.LONG2NUM(sum))
+		go func() {
+			defer close(done)
+
+			runtime.LockOSThread()
+			defer runtime.UnlockOSThread()
+			fmt.Println("sub goroutine", syscall.Gettid())
+
+			time.Sleep(5 * time.Second)
+			// state := 0
+			script := "puts 'Hello, world!'"
+			cscript := C.CString(script)
+			defer C.free(unsafe.Pointer(cscript))
+			fmt.Println("before eval")
+			// C.rb_eval_string_protect(cscript, (*C.int)(unsafe.Pointer(&state)))
+			fmt.Println("G3b")
+		}()
+
+		return done
+	}()
+
+	fmt.Println("before wait")
+	<-done
+	fmt.Println("done")
+
+	return C.VALUE(ruby.LONG2NUM(3))
 }
 
 func main() {
