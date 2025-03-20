7d2d242144c9caeb726431c970c388a1da54bdce
Isamu Mogi <isamu@leafytree.jp>
2025-03-20 14:04:24 +0900

    chore: fix path

--- .devcontainer/on_create_command.bash
+++ .devcontainer/on_create_command.bash
@@ -2,9 +2,9 @@
 
 set -eux -o pipefail
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/.."
 
-./update_content_command.bash
+./.devcontainer/update_content_command.bash
 
 latest_cruby_version=$(bash -ic "rbenv install --list-all" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | tail -n 1)
 bash -ic "rbenv install '$latest_cruby_version'"
